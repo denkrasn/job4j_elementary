ecd21c56a9a02607211fdfd0c29ea54a49e18941
denkrasn <den.job4j@yandex.ru>
2020-06-06 10:12:52 +0300

    4.1. Аргументы [#299420]

--- src/ru/job4j/calculator/ArgMethod.java
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class ArgMethod {
     public static void hello(String name, int age) {
-        System.out.println("Hello, " + name);
+        System.out.println("Hello, " + name + ", " + age);
     }
 
     public static void main(String[] args) {
@@ -10,6 +10,7 @@ public class ArgMethod {
         int age = 33;
 
         ArgMethod.hello(name, age);
+
 
     }
 }
