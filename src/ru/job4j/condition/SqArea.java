18e3c64bde2420a3820cb659bf7a93d441b7b975
denkrasn <den.job4j@yandex.ru>
2020-06-11 10:36:52 +0300

    4.5. Отладка программы в IDEA [#299372]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -4,7 +4,7 @@ public class SqArea {
     public static double square(int p, int k) {
         int h = p / (2 * (k + 1));
         int w = k * h;
-        int s = w + h;
+        int s = w * h;
         return s;
     }
 
--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -2,12 +2,14 @@ package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.converter.Converter;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
 public class SqAreaTest {
 
     @Test
-    public void square() {
+    public void whenp4k1s1() {
         int p = 4;
         int k = 1;
         double expected = 1.0;
