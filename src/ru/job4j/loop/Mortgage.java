e5a7d92b8a36be4e66050a180b0414fab221eda9
denkrasn <den.job4j@yandex.ru>
2020-06-14 15:55:38 +0300

    5.7. Ипотека. [#299464]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+            amount += (amount / 100) * percent;
+            amount -= salary;
+            year++;
+        }
+        return year;
+    }
+
+}
