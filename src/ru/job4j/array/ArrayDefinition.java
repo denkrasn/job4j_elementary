8b5d82f9b3f5440d80e6d0e5df69dcb3bfa766f6
denkrasn <den.job4j@yandex.ru>
2020-06-15 12:49:39 +0300

    6.0.2. Заполнение массива. [#299397]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surname = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Follow";
+        names[1] = "the";
+        names[2] = "white";
+        names[3] = "rabbit...";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
