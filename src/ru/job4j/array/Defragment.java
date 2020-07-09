7b52cfee46d5d779a328fed90904f2911be3a365
denkrasn <den.job4j@yandex.ru>
2020-07-09 14:56:37 +0300

    1. Чтение кода и базовые ошибки [#299539]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -4,15 +4,14 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index + 1; // указатель, на не null ячейку
-                while (point < array.length && array[point] == null)
+                int point = index + 1;
+                while (point < array.length && array[point] == null) {
                     point++;
-                if (point < array.length) {
-                    array[index] = array[point];
-                    array[point] = null;
+                    if (point < array.length) {
+                        array[index] = array[point];
+                        array[point] = null;
+                    }
                 }
-                //переместить первую не null ячуйку
-                // здесь нужен цикл while
             }
             System.out.print(array[index] + " ");
         }
