56568ae6d85ad7cd50e9d4a3e78451b7493b409a
denkrasn <den.job4j@yandex.ru>
2020-07-16 11:01:11 +0300

    3. Перегрузить метод max для трех чисел [#299553]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,13 +1,19 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        boolean condition = left >= right;
-        return condition ? left : right;
+    public static int max(int first, int second) {
+        int result = first > second ? first : second;
+        return result;
     }
 
-    public static void main(String[] args) {
-        int xXx = Max.max(999, 666);
-        System.out.println(xXx);
+    public static int max(int first, int second, int third) {
+        int result = first > max(second, third) ? first : max(second, third);
+        return result;
     }
+
+    public static int max(int first, int second, int third, int four) {
+        int result = first > max(second, third, four) ? first : max(second, third, four);
+        return result;
+    }
+
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.condition;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,6 +10,18 @@ public class MaxTest {
     @Test
     public void max() {
         int result = Max.max(999, 666);
+        assertThat(result, is(999));
+    }
+
+    @Test
+    public void whenMaxThird() {
+        int result = Max.max(666, 999, 333);
+        assertThat(result, is(999));
+    }
+
+    @Test
+    public void whenMaxSecond() {
+        int result = Max.max(999, 444, 222, 777);
         assertThat(result, is(999));
     }
 }
